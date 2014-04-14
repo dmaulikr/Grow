5fc4ae50c6e08b66b3567fceb462df2dac3fb2b5
montanaj <clairejencks@gmail.com>
2014-04-14 14:57:47 -0500

    Initial Commit

---
+++ Grow/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  Grow
+//
+//  Created by Claire Jencks on 4/14/14.
+//  Copyright (c) 2014 Claire Jencks. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ Grow/ViewController.h
@@ -0,0 +1,13 @@
+//
+//  ViewController.h
+//  Grow
+//
+//  Created by Claire Jencks on 4/14/14.
+//  Copyright (c) 2014 Claire Jencks. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+@end
