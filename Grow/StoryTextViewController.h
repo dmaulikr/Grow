2bc548f57ceb186888d593cca5e3ae699baa0081
montanaj <clairejencks@gmail.com>
2014-04-15 18:08:32 -0500

    beginning stages of gameplay, created StoryTextViewController, plotLine.json file

---
+++ Grow/StoryTextViewController.h
@@ -0,0 +1,13 @@
+//
+//  StoryTextViewController.h
+//  Grow
+//
+//  Created by Claire Jencks on 4/15/14.
+//  Copyright (c) 2014 Claire Jencks. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface StoryTextViewController : UIViewController
+
+@end
---
+++ Grow/StoryTextViewController.m
@@ -0,0 +1,36 @@
+//
+//  StoryTextViewController.m
+//  Grow
+//
+//  Created by Claire Jencks on 4/15/14.
+//  Copyright (c) 2014 Claire Jencks. All rights reserved.
+//
+
+#import "StoryTextViewController.h"
+
+@interface StoryTextViewController ()
+
+@property (strong, nonatomic) NSDictionary *jsondataPage;
+@property (strong, nonatomic) IBOutlet UIButton *messageButtonA;
+
+@end
+
+
+
+@implementation StoryTextViewController
+
+
+
+//-(void)loadStoryboard
+//{
+//    NSDictionary *jsondataPage = [NSDictionary new];
+//    NSArray *beginningPointsArray = [jsondataPage objectForKey:@"beginningPoints"];
+//    NSDictionary *character_choicesDictionary = [beginningPointsArray ob]
+//
+//    for (NSDictionary * in beginningPoints)
+//    {
+//        self.messageButtonA.text = [beginningPoints valueForKey:@"messageA"]
+//    }
+//}
+
+@end
