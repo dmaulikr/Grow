3c836a2cd92841ff49e77a8dbc5f88e1c8eaf524
montanaj <clairejencks@gmail.com>
2014-04-17 14:23:45 -0500

    json in working order and buttons populate the next page in the story

--- Grow/StoryTextViewController.m
+++ Grow/StoryTextViewController.m
@@ -9,12 +9,15 @@
 #import "StoryTextViewController.h"
 
 @interface StoryTextViewController ()
+{
+    NSArray *allPagesInStory;
+}
+@property (strong, nonatomic) NSDictionary *jsondataPage;
 
 @property (weak, nonatomic) IBOutlet UITextView *storyText;
-@property (strong, nonatomic) NSDictionary *jsondataPage;
 @property (strong, nonatomic) IBOutlet UIButton *messageButtonA;
-
 @property (weak, nonatomic) IBOutlet UIButton *messageButtonB;
+@property (strong, nonatomic) NSDictionary *pageData;
 @end
 
 
@@ -28,21 +31,40 @@
 
 -(void)loadStoryboard
 {
-    //NSArray *publicTimeline = [NSJSONSerialization JSONObjectWithData:response options:0 error:&jsonParsingError];
-    NSString *filePath = [[NSBundle mainBundle] pathForResource:@"plotLine" ofType:@"json"];
-    NSString *jsonString = [[NSString alloc] initWithContentsOfFile:filePath encoding:NSUTF8StringEncoding error: NULL];
-    //NSData *jsonData = [[NSString alloc] initWithCoder:jsonString];
-    NSDictionary *jsonDataPage = [NSJSONSerialization JSONObjectWithData:[jsonString dataUsingEncoding:NSUTF8StringEncoding] options:0 error:nil];
-    NSArray *allPagesInStory = [jsonDataPage objectForKey:@"allPagesInStory"];
-    NSDictionary *pageData = allPagesInStory[self.pointInStory];
-    
+    if (!self.pageData)
+    {
+        //NSArray *publicTimeline = [NSJSONSerialization JSONObjectWithData:response options:0 error:&jsonParsingError];
+        NSString *filePath = [[NSBundle mainBundle] pathForResource:@"plotLine" ofType:@"json"];
+        NSString *jsonString = [[NSString alloc] initWithContentsOfFile:filePath encoding:NSUTF8StringEncoding error: NULL];
+        //NSData *jsonData = [[NSString alloc] initWithCoder:jsonString];
+        NSDictionary *jsonDataPage = [NSJSONSerialization JSONObjectWithData:[jsonString dataUsingEncoding:NSUTF8StringEncoding] options:0 error:nil];
+        allPagesInStory = [jsonDataPage objectForKey:@"allPagesInStory"];
+        self.pageData = allPagesInStory[self.pointInStory];
+    }
 
-    self.storyText.text = pageData[@"page_text"];
-    NSArray *buttonChoices = pageData[@"choices"];
+    self.storyText.text = self.pageData[@"page_text"];
+    NSArray *buttonChoices = self.pageData[@"choices"];
     
     [self.messageButtonA setTitle:buttonChoices[0][@"buttontext"] forState:UIControlStateNormal];
+    self.messageButtonA.tag = [((NSString *)(buttonChoices[0][@"indexToNextPage"])) intValue];
+
     [self.messageButtonB setTitle:buttonChoices[1][@"buttontext"] forState:UIControlStateNormal];
+   self.messageButtonB.tag = [((NSString *)(buttonChoices[1][@"indexToNextPage"])) intValue];
+
 //put all possible button onto the vc
 //make a FOR LOOPS for the state of the buttons - reference the self.buttonCount, if the count is greater then the count of the number of indexes in the buttonchoices array (in the json) then set the state of the the 3 and for buttons to hidden and vice versa
 }
+- (IBAction)EitherButtonTappedPopulateNextPage:(UIButton*)button
+{
+    self.pageData = allPagesInStory[button.tag];
+    [self loadStoryboard];
+}
+
+
+
+
+
+
+
+
 @end
